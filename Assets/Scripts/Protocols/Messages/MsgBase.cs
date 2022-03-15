b04cdeb731fd46f1a09d024e55abc7f8bec113fd
Mercury <2818676426@qq.com>
2022-03-15 15:56:00 +0800

    modify directory

--- Assets/Scripts/Protocols/Messages/MsgBase.cs
+++ Assets/Scripts/Protocols/Messages/MsgBase.cs
@@ -5,13 +5,35 @@ using System.Linq;
 public class MsgBase
 {
     public string protoName = "null";
-    
+
+    /**
+     * 协议格式为 
+     * struct Message {
+     *     short totalLength; // size(Message) - sizeof(short)
+     *     short msgTypeLength;
+     *     byte[] msgType;
+     *     byte[] infoDetail;
+     * };
+     */
+
+    /**
+     * 将msgBase串行化成JSON
+     * 没有考虑长度等其他信息，还需要编码成协议格式才能发送
+     * TODO: 封装编码方法
+     */
     public static byte[] Encode(MsgBase msgBase)
     {
         string s = JsonUtility.ToJson(msgBase);
         return System.Text.Encoding.UTF8.GetBytes(s);
     }
 
+    /**
+     * 将解码后的结果反串行化成 msgBase
+     * @params protoName 协议名
+     * @params bytes 原字节流
+     * @params offset 从 bytes[offset] 的位置开始解释JSON
+     * @params count 到 bytes[offset + count]的位置停止解释JSON
+     */
     public static MsgBase Decode(string protoName, byte[] bytes, int offset, int count)
     {
         string s = System.Text.Encoding.UTF8.GetString(bytes, offset, count);
@@ -22,46 +44,48 @@ public class MsgBase
     }
 
     /**
-     * 编码协议名（2字节长度+字符串）
-     * 协议格式为 
-     * struct Name {
-     *     short length;
-     *     byte[] str;
-     * };
+     * 编码协议名
+     * @params msgBase 待发送消息
+     * @return bytes 串行化后的协议名长度 + 协议名 byte array
      */
     public static byte[] EncodeName(MsgBase msgBase)
     {
-        //名字bytes和长度
         byte[] nameBytes = System.Text.Encoding.UTF8.GetBytes(msgBase.protoName);
         Int16 len = (Int16)nameBytes.Length;
-        //申请bytes数值
+
         byte[] bytes = new byte[2 + len];
-        //组装2字节的长度信息
+        // 组装2字节的长度信息
         bytes[0] = (byte)(len % 256);
         bytes[1] = (byte)(len / 256);
-        //组装名字bytes
+        // 组装名字bytes
         Array.Copy(nameBytes, 0, bytes, 2, len);
 
         return bytes;
     }
 
-    //解码协议名（2字节长度+字符串）
+    /**
+     * 解码协议名
+     * @params bytes  原字节流
+     * @params offset 从 bytes[offset] 开始提取 msgTypeLength
+     * @return 协议名 msgType
+     * @return_by_out count = sizeof(short) + len(msgType)
+     */
     public static string DecodeName(byte[] bytes, int offset, out int count)
     {
         count = 0;
-        //必须大于2字节
+        // 边界情况考虑，避免数组越界
         if (offset + 2 > bytes.Length)
         {
             return "";
         }
-        //读取长度
+
+        // 读取长度
         Int16 len = (Int16)((bytes[offset + 1] << 8) | bytes[offset]);
-        //长度必须足够
         if (offset + 2 + len > bytes.Length)
         {
             return "";
         }
-        //解析
+
         count = 2 + len;
         string name = System.Text.Encoding.UTF8.GetString(bytes, offset + 2, len);
         return name;
---
+++ Assets/Scripts/Protocols/Util/ProtocolHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+/**
+ +--------------+---+-------------------------+------------------+----
+|    Type  |   |inherit from others | be inherited | be instantiated | 
+|--------------|---|-------------------------+------------------+-----
+| normal   | : |  YES               |    YES       |     YES         |
+| abstract | : |    YES             |    YES       |     NO          |
+| sealed   | : |          YES       |    NO        |     YES         |
+| static   | : |          NO        |    NO        |     NO          |
++--------------+---+-------------------------+------------------+----
+ */
+public static class ProtocolHelper
+{
+    // TODO
+}
